30428a83bd881eb9f1fe3d7992789fce8d6a6bb7
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2025-02-25 19:56:51 -0800

    Updated HTML

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,17 @@
 // sketch.js 
 
 let canvasContainer;
+let color = 0;
 
 function setup() {
     canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
+}
 
-    background(0);
+
+// 
+function draw() {
+    color += 1;
+    background(color % 255);
 }
