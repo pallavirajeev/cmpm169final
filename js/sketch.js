e2f45614c5fc1fc76d7fc361becf13d91ba541ca
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2025-02-25 20:03:41 -0800

    Added sliders

--- js/sketch.js
+++ js/sketch.js
@@ -3,10 +3,26 @@
 let canvasContainer;
 let color = 0;
 
+let sliders = [];
+
+function setupSliders() {
+    sliders.push(document.getElementById("slider1"));
+    sliders.push(document.getElementById("slider2"));
+    sliders.push(document.getElementById("slider3"));
+
+    for (let i = 0; i < sliders.length; i++) {
+        sliders[i].oninput = function() {
+            console.log("Slider " + (i + 1) + " value: " + this.value);
+        }
+    }
+}
+
 function setup() {
     canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
+
+    setupSliders();
 }
 
 
